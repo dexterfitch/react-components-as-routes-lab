f8a31df89f657f1fbd85ab44c6b01712d3a3badb
Dex Fitch <fitch.dexter@gmail.com>
2021-03-04 19:39:22 -0800

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,19 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map(movie =>
+          <div key={movie.title}>
+            <p>Title: <b>{movie.title}</b></p>
+            <p>Running Time: {movie.time}</p>
+            <p>Genres:</p>
+            <ul>
+              {movie.genres.map(genre =>
+                <li>{genre}</li>
+              )}
+            </ul>
+          </div>
+        )}
     </div>
   );
 };
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+const navlink = {
+  padding: '12px',
+  margin: '0 12px 0 0',
+  background: '#ffa5ea',
+  fontFamily: 'sans-serif',
+  fontWeight: 'bold',
+  textDecoration: 'none',
+  color: '#333'
+}
+
 const NavBar = () => {
   return (
-    <div>
-      {/*{code here}*/}
+    <div className="navbar">
+      <NavLink exact to="/" style={navlink} activeStyle={{background: '#f72ec5'}}>Home</NavLink>
+      <NavLink exact to="/movies" style={navlink} activeStyle={{background: '#f72ec5'}}>Movies</NavLink>
+      <NavLink exact to="/directors" style={navlink} activeStyle={{background: '#f72ec5'}}>Directors</NavLink>
+      <NavLink exact to="/actors" style={navlink} activeStyle={{background: '#f72ec5'}}>Actors</NavLink>
     </div>
   );
 };
